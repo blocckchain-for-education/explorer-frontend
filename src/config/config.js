1a9cdb08f714bc259884b5e21a4bc53e8f655f9a
Nguyen Tien Thao <nguyentienthao96@gmail.com>
2020-06-04 08:41:45 +0700

    fix: update api url

--- src/config/config.js
+++ src/config/config.js
@@ -2,16 +2,16 @@ if (process.env.REACT_APP_NODE_ENV !== "production") {
   module.exports = {
     ROOT_URL: "https://live.v-chain.vn/api",
     SAWTOOTH_NODE:"https://v-chain.vn/sawtooth/api",
-    ES_URL:"https://v-chain.vn/elasticsearch",
+    ES_URL:"https://elasticsearch.v-chain.vn/",
     devMode: true,
     websocket_url:"ws:159.65.223.173:8090/subscriptions"
   };
   
 } else {
   module.exports = {
-    ROOT_URL: "sawtooth:8008",
+    ROOT_URL: "https://live.v-chain.vn/api",
     devMode: false,
     websocket_url:"wss:178.128.217.110:8008/subscriptions",
-    ES_URL:"https://v-chain.vn/elasticsearch"
+    ES_URL:"https://elasticsearch.v-chain.vn/"
   };
 }
