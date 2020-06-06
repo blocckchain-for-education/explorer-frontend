91a0b9115ff78836804a1b0685fa13e3664fece5
Nguyen Tien Thao <nguyentienthao96@gmail.com>
2020-06-06 10:43:11 +0700

    es

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ if (process.env.REACT_APP_NODE_ENV !== "production") {
   module.exports = {
     ROOT_URL: "https://live.v-chain.vn/api",
     SAWTOOTH_NODE:"https://v-chain.vn/sawtooth/api",
-    ES_URL:"https://elasticsearch.v-chain.vn/",
+    ES_URL:"https://elasticsearch.v-chain.vn",
     devMode: true,
     websocket_url:"ws:159.65.223.173:8090/subscriptions"
   };
@@ -12,6 +12,6 @@ if (process.env.REACT_APP_NODE_ENV !== "production") {
     ROOT_URL: "https://live.v-chain.vn/api",
     devMode: false,
     websocket_url:"wss:178.128.217.110:8008/subscriptions",
-    ES_URL:"https://elasticsearch.v-chain.vn/"
+    ES_URL:"https://elasticsearch.v-chain.vn"
   };
 }
